84730bca0096712b44fba92bb7ad235cb972939e
sebaspapu <sebasnarutoxd@gmail.com>
2025-07-23 00:06:28 -0500

    fix: main

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
-
 // main.jsx
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Quiz from "./pages/quiz/Quiz";
 import NotFound from "./pages/not-found/NotFound";
-@@ -13,29 +14,40 @@ import Queratocono from "./pages/enfermedades/queratocono/Queratocono";
+import Heart from "./pages/heart/Heart";
+import HighBloodPressure from "./pages/heart/high-blood-pressure/HighBloodPressure";
+import LowBloodPressure from "./pages/heart/low-blood-pressure/LowBloodPressure";
+import "./index.css";
+import Layout from "./layout/Layout";
+import Login from "./pages/login/Login";
+import Queratocono from "./pages/enfermedades/queratocono/Queratocono";
 import Conjuntivitis from "./pages/enfermedades/conjuntivitis/Conjuntivitis";
 import Glaucoma from "./pages/enfermedades/glaucoma/Glaucoma";
 import Cataratas from "./pages/enfermedades/cataratas/Cataratas";
